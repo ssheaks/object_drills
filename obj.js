dba24e450921aedeaa3f07ce9810c4b7f182d956
ssheaks <sarah.sheaks@gmail.com>
2020-05-04 18:46:21 -0700

    added decode obj fxn

--- obj.js
+++ obj.js
@@ -75,3 +75,75 @@ peeps2.forEach(person => ('boss' in person) ?
     console.log(`${person.jobTitle} ${person.name} reports to ${person.boss}.`):
     console.log(`${person.jobTitle} ${person.name} doesn't report to anyone`)
 );
+
+// 6. Cracking the code
+// Redo your Cracking the Code problem from String Drills but this time use an object as your cipher. Additionally, create a decodeWords function that utilizes your decode function to accept a single string of words, and then return the fully decoded message as a string.
+// Cracking the code
+// A code has been invented which replaces each character in a sentence with a five letter word. The first letter of each encoded word determines which of the remaining four characters contains the decoded character according to this table:
+
+// First letter	Character number
+// a	2
+// b	3
+// c	4
+// d	5
+// So for example, the encoded word 'cycle' would be decoded to the character 'l'. This is because the first letter is a 'c', so you look for the fourth character, which is 'l'.
+
+// If the first letter of the encoded word isn't 'a', 'b', 'c', or 'd' (for example 'mouse') this should be decoded to a space.
+
+// Write a function called decode which takes an encoded word as an argument, and returns the correct decoded character.
+
+// Use your function to decode the following message: 'craft block argon meter bells brown croon droop'.
+
+const objMessage = {
+    1: 'craft',
+    2: 'block',
+    3: 'argon',
+    4: 'meter',
+    5: 'bells',
+    6: 'brown',
+    7: 'croon',
+    8: 'droop'
+};
+
+function decodeObj(obj) {
+    let decoded = '';
+    for (const key in obj) {
+        if(obj[key].charAt(0) === 'a') {
+             decoded = decoded + `${obj[key].charAt(1)}`;
+             console.log(decoded);
+        } else if(obj[key].charAt(0) === 'b') {
+            decoded = decoded + `${obj[key].charAt(2)}`;
+        } else if(obj[key].charAt(0) === 'c') {
+            decoded = decoded + `${obj[key].charAt(3)}`;
+        } else if(obj[key].charAt(0) === 'd') {
+            decoded = decoded + `${obj[key].charAt(4)}`;
+        } else {
+            decoded = decoded + ' ';
+        }
+    }
+    console.log(decoded);
+    return decoded;
+}
+decodeObj(objMessage);
+//string version
+
+function decode(str) {
+    const strArr = str.split(" ");
+    const letterArr = strArr.map(word => {
+        if(word.charAt(0) === 'a') {
+            return word.charAt(1);
+        } else if(word.charAt(0) === 'b') {
+            return word.charAt(2);
+        } else if(word.charAt(0) === 'c') {
+            return word.charAt(3);
+        } else if(word.charAt(0) === 'd') {
+            return word.charAt(4);
+        } else {
+            return ' ';
+        }
+    });
+    console.log(letterArr.join(''));
+    return letterArr.join('');
+}
+const message = 'craft block argon meter bells brown croon droop';
+decode(message);
